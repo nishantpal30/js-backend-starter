b0706b56bdaa385b9fdc3a3cda4ab314d478a2b8
Nishant Pal <nishantpal708853@gmail.com>
2026-08-29 20:37:28 +0530

    Starter Packages

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { serverConfig } from "./config/server.config.ts";
+import { connectDB } from "./config/db.config.ts";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+ 
+
+
+// middleware
+app.listen(serverConfig.PORT, async ()=>{
+
+ console.log(`Server Running Sucessfully at PORT ${serverConfig.PORT}`);
+ connectDB();
+});
